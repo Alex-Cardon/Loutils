7d12b28e9ff4d64cae23e860e04ee2b3eb745503
Alex-Cardon <pierrealexandre.cardon@gmail.com>
2021-06-09 11:34:42 +0200

    insert user

---
+++ BACK/app/validations/validate.js
@@ -0,0 +1,50 @@
+const validate = {
+
+/*
+ * Cette méthode est un factory de middleware
+ * Elle sert à pouvoir récéptionner le schema (injection de dépendance)
+ * @param {object} schema 
+ * @param {object} schema 
+*/
+
+ queryString: (schema) => {
+
+  return async (request, response, next) => {
+
+try {
+await schema.validateAsync(request.query);
+next();
+} catch (error) {
+ return response.status(400).json({ error: error.details[0].message });
+}
+}
+},
+
+ body: (schema) => {
+
+  return async (request, response, next) => {
+
+try {
+await schema.validateAsync(request.body);
+next();
+} catch (error) {
+ return response.status(400).json({ error: error.details[0].message });
+}
+}
+},
+
+ params: (schema) => {
+
+  return async (request, response, next) => {
+
+try {
+await schema.validateAsync(request.params);
+next();
+} catch (error) {
+ return response.status(400).json({ error: error.details[0].message });
+}
+}
+},
+};
+
+module.exports = validate;
