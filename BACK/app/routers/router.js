d487205468abc1220b59e9c1e6f9f60de0d68c62
Alex-Cardon <pierrealexandre.cardon@gmail.com>
2021-06-21 10:42:48 +0200

    back repared

--- BACK/app/routers/router.js
+++ BACK/app/routers/router.js
@@ -15,7 +15,7 @@ const validUserSettings = require('../middlewares/validUserSettings');
 const ratingController = require('../controllers/ratingController');
 const pictureController = require('../controllers/pictureController');
 const bookingController = require('../controllers/bookingController');
-const radiusController = require('../controllers/radiusController');
+const configController = require('../controllers/configController');
 const modoContoller = require('../controllers/modoContoller');
 
 const schemas = require('../validations/schemas');
@@ -29,7 +29,7 @@ const imageUpload = multer({
         limits: { fileSize: maxSize }
     });
 
-router.get('/radius', radiusController.radiusList);
+router.get('/radius', configController.radiusArray);
 
 router.get('/randads',adController.getRandAds)/* JSDOC ok */;
         
