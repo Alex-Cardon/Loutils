f997961a15022263b0f4ef3eef015d35e7169378
CEDRIC-code1982 <cedric.pineau007@gmail.com>
2021-06-23 19:57:15 +0000

    test code Ségo

--- FRONT/src/containers/Content/index.js
+++ FRONT/src/containers/Content/index.js
@@ -3,10 +3,10 @@ import { connect } from 'react-redux';
 import Content from 'src/components/Content';
 // TODO en cpours de construction
 const mapStateToprops = (state) => ({
-  id: state,
-  title: state,
-  description: state,
-  price: state,
+  id: state.announcements.id,
+  title: state.announcements.title,
+  description: state.announcements.description,
+  price: state.announcements.price,
 });
 
 export default connect(mapStateToprops)(Content);
