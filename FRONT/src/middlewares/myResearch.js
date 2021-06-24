3ef9779f0d698d45dc1deeee9b6d025784ac51cf
Segolene-Danjou <segolene.danjou@gmail.com>
2021-06-24 08:35:35 +0200

    push du matin

--- FRONT/src/middlewares/myResearch.js
+++ FRONT/src/middlewares/myResearch.js
@@ -9,7 +9,7 @@ import {
 const myResearchMiddleware = (store) => (next) => (action) => {
   switch (action.type) {
     case GET_MY_RESEARCH:
-      axios.get(`GET http://localhost:3000/search`)
+      axios.get(`http://ec2-3-237-39-254.compute-1.amazonaws.com:3000/savedResearch`)
         .then((response) => {
           console.log('response de myResearch', response.data)
           store.dispatch(getMyResearchSuccess(response.data));
