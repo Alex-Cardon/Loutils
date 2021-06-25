8e69f6641a2e11b53bd5e4cd8139fa036ce1a1b9
Alex-Cardon <pierrealexandre.cardon@gmail.com>
2021-06-25 23:45:03 +0200

    client modif

--- BACK/app/client.js
+++ BACK/app/client.js
@@ -1,8 +1,8 @@
 
 const { Pool } = require('pg');
 
-module.exports = new Pool(/*{
+module.exports = new Pool({
     host: 'loutilsdb.cp20qy5y1lnp.us-east-1.rds.amazonaws.com',
     user: 'postgres',
     password: 'UKky1ypHokT3hl5ERNOr'
-  }*/);
+  });
