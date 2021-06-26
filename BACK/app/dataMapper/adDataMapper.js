1e2b154d11a1ab5391346d58b6e555bffd5dc1df
Alex-Cardon <pierrealexandre.cardon@gmail.com>
2021-06-26 11:27:05 +0200

    develop ad repair

--- BACK/app/dataMapper/adDataMapper.js
+++ BACK/app/dataMapper/adDataMapper.js
@@ -75,7 +75,11 @@ module.exports = {
 
      AND LOWER("ad"."title") LIKE LOWER($1) 
      
-     AND "category"."name"= $2 ORDER BY "created_at" DESC`, ['%' + title + '%', category]);
+     AND "category"."name"= $2
+
+     AND "moderated" = TRUE 
+
+     ORDER BY "created_at" DESC`, ['%' + title + '%', category]);
 
 
         return (result.rows);
@@ -103,7 +107,11 @@ module.exports = {
     
     WHERE "ad"."postcode" IN (` + postcode.join(',') + `)
 
-     AND LOWER("ad"."title") LIKE LOWER($1) ORDER BY "created_at" DESC`, ['%' + title + '%']);
+     AND LOWER("ad"."title") LIKE LOWER($1)
+     
+     AND "moderated" = TRUE
+     
+     ORDER BY "created_at" DESC`, ['%' + title + '%']);
 
         return (result.rows);
     },
