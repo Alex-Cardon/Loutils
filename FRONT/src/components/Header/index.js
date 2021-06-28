526fb95b1ca3bd49fb6ae0f569c6bdeefdbaa6e4
Alex-Cardon <pierrealexandre.cardon@gmail.com>
2021-06-28 22:10:35 +0200

    menu burger

--- FRONT/src/components/Header/index.js
+++ FRONT/src/components/Header/index.js
@@ -2,13 +2,36 @@ import React from 'react';
 
 import { NavLink } from 'react-router-dom';
 
-
+import LoginForm from 'src/components/LoginForm'
 import logo from 'src/components/Header/loutils_noir.svg';
 
 import './styles.scss';
 
 const Header = () => (
   <div className="header">
+
+<nav role="navigation">
+  <div id="menuToggle">
+
+    <input type="checkbox" />
+    
+    <span></span>
+    <span></span>
+    <span></span>
+    
+ 
+    <ul id="menu">
+      <a href="/SignupPage"><li>Inscription</li></a>
+      <a href="#"><li>About</li></a>
+      <a href="#"><li>Info</li></a>
+      <a href="#"><li>Contact</li></a>
+      <a href="https://erikterwan.com/" target="_blank"><li>Show me more</li></a>
+    </ul>
+  </div>
+    < LoginForm />
+
+</nav>
+
     <NavLink
       className='header-navlink'
       exact
