6c882d407a74c434658c50730c960eafacae1963
Segolene-Danjou <segolene.danjou@gmail.com>
2021-07-01 19:30:33 +0200

    toastify inscription ok

--- FRONT/src/components/SignupPage/index.js
+++ FRONT/src/components/SignupPage/index.js
@@ -2,17 +2,24 @@ import React from 'react';
 import { Link, Redirect } from 'react-router-dom';
 
 import Proptypes from "prop-types";
+import { toast } from 'react-toastify';//
+import 'react-toastify/dist/ReactToastify.css'//
 // j'importe mon composant connecté
 //import Header from 'src/containers/Header';<Header />
 import ConnectedSettingsField from 'src/containers/SignupPage/SettingsField';
 //! 1 je fais mon component maintenant direction le sous composant SettingsField
 
 import './styles.scss';
+toast.configure()//
 
 const Signup = ({ 
   handleFormSubmit, 
   signUp,
-}) => (
+}) => {
+  const notify = () => {
+    toast.success('Votre compte a bien été créer. Veuillez cliquer sur le lien de confirmation envoyé par mail. Regardez bien vos spams !', {position: toast.POSITION.TOP_RIGHT} )
+  }//juste au dessus du retur !!!!!//
+return(
   <div className="signup-page">
     
     <h2>Créer votre compte</h2>
@@ -53,14 +60,15 @@ const Signup = ({
         
       />
 
-      <button className="settings__submit" type="submit" >Valider</button>
+      <button className="settings__submit" type="submit" onClick={notify}>Valider</button>
       {signUp && (<Redirect from="/SignupPage" to="/" />)}
 
 <Link to="/" className="backToHomeLink">Annuler</Link>
 
     </form>
   </div>
-);
+)
+};
 
 Signup.proptypes = {
   handleFormSubmit: Proptypes.func.isRequired,
